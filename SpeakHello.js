6c069e6cae98d0547e1656225db59cf615a7b8ad
joshua5512 <96761572+joshua5512@users.noreply.github.com>
2022-01-29 20:00:49 +1000

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -11,3 +11,13 @@
 
 }) (window);
 
+(function(window) {
+  
+  var byeSpeaker = {};
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = function speak(name) {
+  console.log(speakWord + " " + name);
+  }
+
+  window.byeSpeaker = byeSpeaker;
+}) (window);
