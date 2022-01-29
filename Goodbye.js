519ecfd59ec908f3f9893d77cf7dd645c1e431c4
joshua5512 <96761572+joshua5512@users.noreply.github.com>
2022-01-29 20:02:43 +1000

    Update Goodbye.js

--- Goodbye.js
+++ Goodbye.js
@@ -1,9 +1,1 @@
-(function(window) {
-  var byeSpeaker = {};
-  var speakWord = "Good Bye";
-  byeSpeaker.speak = function speak(name) {
-  console.log(speakWord + " " + name);
-  }
 
-  window.byeSpeaker = byeSpeaker;
-}) (window);
